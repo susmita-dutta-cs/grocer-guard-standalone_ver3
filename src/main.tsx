4d0cfb900d2d6b84a62fb2b796e7b2bd3674080c
susmita-dutta-cs <dutta.or.susmita@gmail.com>
2026-03-14 23:33:25 +0100

    Fix: Add back App import to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 import { I18nProvider } from './hooks/useI18n.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
